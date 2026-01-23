a2c1e64963126e12be653d1536508ad7ea8a56ba
Anasofia10289 <egiovanny-20@hotmail.com>
2026-01-22 20:22:08 -0500

    se crea diccionario

---
+++ main.py
@@ -0,0 +1,14 @@
+contador=0
+
+'''while contador<5:
+    print(f"Estoy en la vuelta: {contador}")
+    contador=contador+1'''
+
+for contador in range (1,6):
+
+    diccionario={}
+    diccionario["id"]=int(input("Digita el id del empleado: "))
+    diccionario["id"]=nombres=input("Digita los nombres del empleado")
+    diccionario["id"]=documento=input("Digita el documento: ")
+    diccionario["id"]=correo=input("Digita el correo: ")
+    diccionario["id"]=contraseña=input("Digita la contraseña: ")
